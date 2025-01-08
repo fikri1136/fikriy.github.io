577e817e53349c159c7b220e23e366ca965dc232
fikri1136 <mfikrif06@gmail.com>
2025-01-08 10:51:59 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,199 @@
+const teams = [
+  {
+    name: "Muhammad Khaidir Fahram",
+    image:
+      "https://fahram.com//img/profile-photos/jzZj0jPuEKTWwY9a8lxo7pQhA29zxFpVQd7xl4BZ.jpg",
+    email: "fahram@fahram.com",
+    link: [
+      {
+        name: "instagram",
+        link: "https://instagram.com/khaidir.fahram",
+      },
+      {
+        name: "facebook",
+        link: "https://facebook.com/khaidir.fahram",
+      },
+      {
+        name: "globe",
+        link: "https://fahram.com/",
+      },
+    ],
+  },
+  {
+    name: "Achmad Maulana Ichsan",
+    image:
+      "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+    email: "fahram@fahram.com",
+    link: [
+      {
+        name: "instagram",
+        link: "https://instagram.com/a.iichsan",
+      },
+      {
+        name: "globe",
+        link: "https://icsan.my.id/",
+      },
+    ],
+  },
+  {
+    name: "Marcella Setyana Putri",
+    image:
+      "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+    email: "fahram@fahram.com",
+    link: [
+      {
+        name: "instagram",
+        link: "https://instagram.com/a.iichsan",
+      },
+      {
+        name: "globe",
+        link: "https://icsan.my.id/",
+      },
+    ],
+  },
+  {
+    name: "Fredo Pradana",
+    image:
+      "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+    email: "msetyaningputri@gmail.com",
+    link: [
+      {
+        name: "instagram",
+        link: "https://instagram.com/mqrcelllaaaa",
+      },
+      {
+        name: "globe",
+        link: "https://icsan.my.id/",
+      },
+    ],
+  },
+  {
+    name: "Rheina Wardiana Hafirah",
+    image:
+      "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+    email: "rheinawardiana317@gmail.com",
+    link: [
+      {
+        name: "instagram",
+        link: "https://instagram.com/na_rrrrr44",
+      },
+      {
+        name: "globe",
+        link: "https://icsan.my.id/",
+      },
+    ],
+  },
+  {
+    name: "Muhammad Fikri Firdaus",
+    image:
+      "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+    email: "mfikrif06@gmail.com",
+    link: [
+      {
+        name: "instagram",
+        link: "https://instagram.com/m_f1kr1n",
+      },
+      {
+        name: "globe",
+        link: "https://fikriy.my.id/",
+      },
+    ],
+  },
+
+  {
+    name: "Sandi Nourmann",
+    image:
+      "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+    email: "mfikrif06@gmail.com",
+    link: [
+      {
+        name: "instagram",
+        link: "https://instagram.com/sndiinrmn",
+      },
+      {
+        name: "globe",
+        link: "https://icsan.my.id/",
+      },
+    ],
+  },
+  {
+    name: "Aisyah",
+    image:
+      "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+    email: "mfikrif06@gmail.com",
+    link: [
+      {
+        name: "instagram",
+        link: "https://instagram.com/a.iichsan",
+      },
+      {
+        name: "globe",
+        link: "https://icsan.my.id/",
+      },
+    ],
+  },
+  {
+    name: "Rozak Al Baidawi",
+    image:
+      "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+    email: "rozakalbaidawi@gmail.com",
+    link: [
+      {
+        name: "instagram",
+        link: "https://instagram.com/_rzkalbdwi",
+      },
+      {
+        name: "globe",
+        link: "https://rozak19.my.id/",
+      },
+    ],
+  },
+  {
+    name: "Naufal Laudza",
+    image:
+      "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+    email: "mfikrif06@gmail.com",
+    link: [
+      {
+        name: "instagram",
+        link: "https://instagram.com/naufalaudzaa",
+      },
+      {
+        name: "globe",
+        link: "https://naufal19.my.id/",
+      },
+    ],
+  },
+  {
+    name: "Hanif Dwi Novario",
+    image:
+      "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+    email: "mfikrif06@gmail.com",
+    link: [
+      {
+        name: "instagram",
+        link: "https://instagram.com/a.iichsan",
+      },
+      {
+        name: "globe",
+        link: "https://icsan.my.id/",
+      },
+    ],
+  },
+  {
+    name: "Abdulrahman Arafah Sunarto",
+    image:
+      "https://bprpedungan.com/wp-content/uploads/2017/08/Person-placeholder.jpg",
+    email: "mfikrif06@gmail.com",
+    link: [
+      {
+        name: "instagram",
+        link: "https://instagram.com/a.iichsan",
+      },
+      {
+        name: "globe",
+        link: "https://icsan.my.id/",
+      },
+    ],
+  },
+];
