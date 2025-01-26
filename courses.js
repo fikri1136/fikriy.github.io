e0e4bfd8060146973ad5d62b2e32c7590afc118c
fikri1136 <mfikrif06@gmail.com>
2025-01-26 15:12:36 +0700

    Update courses.js

--- courses.js
+++ courses.js
@@ -1,20 +1,24 @@
 const container = document.getElementsByClassName("container")[0];
-function generateBox(team) {
+function generateBox(course) {
   const box = document.createElement("div");
   box.className = "box";
   container.appendChild(box);
   const img = document.createElement("img");
+
   img.className = "media";
-  img.src = team.image;
+  img.src = course.image;
   box.appendChild(img);
-  const title = document.createElement("h4");
-  title.innerText = team.name;
+
+  const title = document.createElement("h3");
+  title.innerText = course.title;
   box.appendChild(title);
+
   const path = document.createElement("p");
-  path.innerText = team.path;
+  path.innerText = course.path;
   box.appendChild(path);
 }
 
 fetch("https://fahram.dev/api/v2/courses")
   .then((response) => response.json())
+
   .then((data) => data.data.forEach((course) => generateBox(course)));
